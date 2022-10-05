fe1b6c288580f3b0d12cc1bacfc8ceb2c7787a01
Dmitrii <directsdi@gmail.com>
2022-10-05 16:49:51 +0500

    четное\нечетное

---
+++ DZ_003/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите число:   ");
+int A = Convert.ToInt32 (Console.ReadLine());
+if (A % 2 == 0)
+{
+    Console.WriteLine (A + "  Ура, число четное!");
+}
+else
+{
+    Console.WriteLine (A + "  Число нечетное!");
+}
