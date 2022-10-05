bf6e5974c45daa590e5ee69205e66f20d947bcce
Dmitrii <directsdi@gmail.com>
2022-10-05 12:41:49 +0500

    сравнение двух чисел

---
+++ DZ_001/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine ("Введите первое число ");
+int A = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Введите второе число ");
+int B = Convert.ToInt32 (Console.ReadLine());
+int max = A;
+if (B > max)
+{
+    Console.Write(B);
+    Console.Write(" больше ");
+    Console.WriteLine(A);
+    }
+else
+{
+    Console.Write(A);
+    Console.Write(" больше ");
+    Console.WriteLine(B);
+}
