12475bfbfdc45558c9380d795cccb8098dc91751
Dmitrii <directsdi@gmail.com>
2022-10-06 21:04:16 +0500

    вывод четных числе от N до -N

---
+++ DZ_004/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+string N = Console.ReadLine ();
+int N1 = int.Parse (N);
+int size = 0-N1;
+while (size <= N1)
+{
+    if (size%2==0)
+    {
+        Console.Write (size + ", ");
+    }
+    size = size +1;
+}
